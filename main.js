b23809a12b956d30b4c9908be53d5f8fb0236b67
maticht <74956969+maticht@users.noreply.github.com>
2021-10-17 11:56:59 +0300

    HomeWork-3

--- main.js
+++ main.js
@@ -1,78 +1,107 @@
-// const name = 'Scorpion';
-//
-// function getMessage(name, lastName) {
-//     console.log(name + ' ' + lastName + ' - Fight...')
-// }
-// getMessage('Sonya', 'Blade');
-// getMessage(name);
-
-// function createFullName(name, lastName, secondName) {
-//     const result = name + ' ' + lastName + ' ' + secondName;
-//     console.log(1)
-//     return result;
-//     console.log(2)
-// }
-// const fullName = createFullName('Матвей','Трейвас','Андреевич');
-// console.log(fullName)
-
-// const massage = ['Hello', true, 2021,];
-// console.log(massage[1])
-// massage.push(' World')
-// console.log(massage)
-// const str = massage[0] + massage[3]
-// console.log(str)
-
 const player1 = {
-    name: 'Scorpion',
-    hp: 90,
+    player: 1,
+    name: 'SCORPION',
+    hp: 100,
     img:'http://reactmarathon-api.herokuapp.com/assets/scorpion.gif',
     weapon: ['sword', 'ninjato'],
-    attack: function () {
+    attack: function() {
         console.log(player1.name + 'Fight...')
     }
 };
 player1.attack();
 
 const player2 = {
-    name: 'Subzero',
-    hp: 90,
+    player: 2,
+    name: 'SUBZERO',
+    hp: 100,
     img:'http://reactmarathon-api.herokuapp.com/assets/subzero.gif',
     weapon: ['ice sword', 'ice scepter'],
-    attack: function () {
+    attack: function() {
         console.log(player2.name + 'Fight...')
     }
 };
 player2.attack();
 
-function createPlayer(playerOrder, object) {
+const aren = document.querySelector('.arenas');
+const $randomButton = document.querySelector('.button');
 
-    const player = document.createElement('div');
-        player.classList.add(playerOrder)
-    document.querySelector('div.arenas').appendChild(player)
+function createElement(tag, className) {
+    const $tag = document.createElement(tag);
+    if (className) {
+        $tag.classList.add(className)
+    }
+    return $tag;
+}
+function createPlayer(object) {
 
-    const progressbar = document.createElement('div');
+    const player = createElement('div', 'player'+object.player);
+
+    const progressbar = createElement('div', 'progressbar');
         progressbar.classList.add('progressbar')
             player.appendChild(progressbar)
 
-    const life = document.createElement('div');
+    const life = createElement('div', 'life');
         life.classList.add('life')
             progressbar.appendChild(life)
     life.style.width = object.hp + '%'
 
-    const name = document.createElement('div');
+    const name = createElement('div', 'name');
         name.classList.add('name')
             progressbar.appendChild(name)
     name.innerText = object.name
 
-    const character = document.createElement('div');
+    const character = createElement('div', 'character');
         character.classList.add('character')
             player.appendChild(character)
 
-    const img = document.createElement('img');
+    const img = createElement('img');
         img.classList.add('img')
             character.appendChild(img)
     img.src = object.img
-};
+    return player;
+}
 
-createPlayer('player1', player1)
-createPlayer('player2', player2)
+function changeHP(player){
+    const $playerLife = document.querySelector('.player' + player.player + ' .life');
+    player.hp -= Math.ceil(Math.random() * 20);
+    $playerLife.style.width = player.hp + '%';
+    if (player.hp < 0){
+        player.hp = 0
+    }
+}
+function hitPlayers() {
+    changeHP(player1);
+    changeHP(player2);
+    if (isDraw()) {
+        console.log('draw');
+        aren.appendChild(endBattle('DRAW'));
+    } else {
+        let winner = hasWinner();
+        console.log('winner is ' + winner);
+        if (winner) aren.appendChild(endBattle(winner + ' WINS'));
+    }
+}
+       function hasWinner() {
+           if (player1.hp <= 0) return player2.name;
+           if (player2.hp <= 0) return player1.name;
+       }
+            function isDraw() {
+                return (player1.hp<=0) && (player2.hp<=0);
+            }
+                function endBattle(text) {
+                    const $title = createElement('div','loseTitle');
+                    $title.innerText = text;
+                    $randomButton.disabled = true;
+                    return $title;
+                }
+
+$randomButton.addEventListener('click', function () {
+    hitPlayers()
+});
+
+aren.appendChild(createPlayer(player1));
+aren.appendChild(createPlayer(player2));
+
+
+
+
